7aa3f263c86bde099992c8a0f736516921a60013
Pravin Pawar <pravinpawar2114@gmail.com>
2026-08-22 23:51:11 +0530

    22/08/2026

--- src/Java_Basics/UserInput.java
+++ src/Java_Basics/UserInput.java
@@ -7,7 +7,29 @@ import java.io.InputStreamReader;
 public class UserInput {
     public static void main(String[] args) throws IOException
     {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter first number: ");
+        int num1 = Integer.parseInt(br.readLine());
+
+        System.out.print("Enter second number: ");
+        int num2 = Integer.parseInt(br.readLine());
+
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
+
+
+
+
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter Age: ");
         String s1 = br.readLine();
         int age = Integer.parseInt(s1);
@@ -16,6 +38,6 @@ public class UserInput {
         System.out.println("Enter rollno.: ");
         int roll = Integer.parseInt(br.readLine());
 
-        System.out.println("My Roll No. : " + roll);
+        System.out.println("My Roll No. : " + roll);*/
     }
 }
