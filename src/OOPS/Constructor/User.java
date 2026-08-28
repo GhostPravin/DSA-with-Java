4de816ebc5081baec402f4ed52799b0ac59acc65
Pravin Pawar <pravinpawar2114@gmail.com>
2026-08-28 23:57:44 +0530

    28/08/2026

--- src/OOPS/Constructor/User.java
+++ src/OOPS/Constructor/User.java
@@ -18,7 +18,7 @@ public class User {
         this.password = password;
         this.email = email;
         System.out.println("Para");
-        //display();
+        display();
     }
 
    /* void setUser(String u,String p, String e)
