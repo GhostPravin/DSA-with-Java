fe0bd1ec3b0f018b1fc37f44c1799e1e563a5f9f
Pravin Pawar <pravinpawar2114@gmail.com>
2026-08-31 21:58:15 +0530

    31/08/2026

---
+++ src/Phase1_Condition/NumberSign.java
@@ -0,0 +1,28 @@
+package Phase1_Condition;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberSign {
+    public static void main(String[] args) throws IOException {
+        int a;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Emter a Number: ");
+        a = Integer.parseInt(br.readLine());
+
+        if (a == 0)
+        {
+            System.out.println("Numer is Zero");
+        }
+        else if (a < 0)
+        {
+            System.out.println("Number is Negative");
+        }
+        else
+        {
+            System.out.println("Number is positive");
+        }
+
+    }
+}
